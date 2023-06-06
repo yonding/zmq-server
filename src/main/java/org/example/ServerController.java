d271fa5361072c61c44731c2aced874e360502bd
yonding <uwrgoy7584@khu.ac.kr>
2023-06-06 16:27:43 +0900

    Create ServerController

---
+++ src/main/java/org/example/ServerController.java
@@ -0,0 +1,44 @@
+package org.example;
+
+import org.example.server.*;
+
+import java.util.Scanner;
+
+public class ServerController {
+    public static void main(String[] args) throws Exception {
+
+        int num = -1;
+        Scanner sc = new Scanner(System.in);
+        while (num != 0) {
+            System.out.println("========================");
+            System.out.println(" 0 : Quit");
+            System.out.println(" 1 : ReqRepBasicServer");
+            System.out.println(" 2 : PubSubBasicServer");
+            System.out.println(" 3 : PubSubAndPullPushServer");
+            System.out.println(" 4 : PubSubAndPullPushServerV2");
+            System.out.println(" 5 : DealerRouterAsyncServer");
+            System.out.println("========================");
+
+            num = sc.nextInt();
+            switch (num) {
+                case 1:
+                    ReqRepBasicServer.start();
+                    break;
+                case 2:
+                    PubSubBasicServer.start();
+                    break;
+                case 3:
+                    PubSubAndPullPushServer.start();
+                    break;
+                case 4:
+                    PubSubAndPullPushServerV2.start();
+                    break;
+                case 5:
+                    DealerRouterAsyncServer.start();
+                    break;
+                default:
+                    System.out.println("1~5 중에 선택해주세요.\n");
+            }
+        }
+    }
+}
