5fdfc74be50e6429a8f7e0474ba122fa5bbcecf0
yonding <uwrgoy7584@khu.ac.kr>
2023-06-20 18:07:34 +0900

    Update comments

--- src/main/java/org/example/server/ReqRepBasicServer.java
+++ src/main/java/org/example/server/ReqRepBasicServer.java
@@ -10,21 +10,23 @@ import static java.lang.Thread.sleep;
 public class ReqRepBasicServer {
     public static void start() throws Exception {
         try (ZContext context = new ZContext()) {
-            // Socket to talk to clients
+            // 클라이언트와 데이터를 주고받을 수 있도록 소켓을 생성한다.
+            // 클라이언트로부터 데이터를 받으면 보내는 소켓 타입은 REP이다.
             ZMQ.Socket socket = context.createSocket(SocketType.REP);
-            socket.bind("tcp://*:5555");
+            socket.bind("tcp://*:5551");
 
             while (true) {
-                // Wait for next request from client
-                byte[] reply = socket.recv(0);
+                // 클라이언트로부터 데이터를 받을 때까지 기다린다. (default flag: 0)
+                byte[] reply = socket.recv();
+                // 클라이언트로부터 받은 메세지를 출력한다.
                 System.out.println("Received request: " + new String(reply, ZMQ.CHARSET));
 
-                //  Do some 'work'
+                // 잠시 동작 중지
                 sleep(1000);
 
-                // Send reply back to client
+                // 클라이언트에게 메세지 전송한다. (default flag: 0)
                 String response = "World";
-                socket.send(response.getBytes(ZMQ.CHARSET), 0);
+                socket.send(response.getBytes(ZMQ.CHARSET));
 
             }
         }
