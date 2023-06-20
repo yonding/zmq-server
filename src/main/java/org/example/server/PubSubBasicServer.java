7251920099fb246c9a774126a74a805e03f5548f
yonding <uwrgoy7584@khu.ac.kr>
2023-06-20 18:07:49 +0900

    Update comments

--- src/main/java/org/example/server/PubSubBasicServer.java
+++ src/main/java/org/example/server/PubSubBasicServer.java
@@ -12,22 +12,24 @@ public class PubSubBasicServer
         try (ZContext context = new ZContext()) {
             System.out.println("Publishing updates at weather server...");
 
+            // Publish 타입의 소켓을 생성한다.
             ZMQ.Socket publisher = context.createSocket(SocketType.PUB);
+            // 해당 소켓을 포트번호 5556에 bind한다.
             publisher.bind("tcp://*:5556");
 
-            //  Initialize random number generator
+            // 시드를 현재 시간으로 설정하여 Random 객체를 생성한다.
             Random srandom = new Random(System.currentTimeMillis());
             while (true) {
+                // zipcode, temperature, relhumidity를 랜덤수로 초기화한다.
                 int zipcode = 10000 + srandom.nextInt(10000);
                 int temperature = srandom.nextInt(215) - 80 + 1;
                 int relhumidity = srandom.nextInt(50) + 10 + 1;
 
-                //  Send message to all subscribers
+                // 모든 Subscribers에게 데이터를 업데이트한다. (default flag: 0)
                 String update = String.format(
                         "%05d %d %d", zipcode, temperature, relhumidity
                 );
-
-                publisher.send(update, 0);
+                publisher.send(update);
             }
         }
     }
